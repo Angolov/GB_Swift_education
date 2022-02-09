598c86127fe8fa6fe574b7952ffbe7da123c19fd
Angolov <anton-golovatyy@yandex.ru>
2022-02-09 21:50:53 +0300

    Частично сделано задание 16 из задачника по UIKit

---
+++ 2. Курс Пользовательский интерфейс iOS-приложений/Задачи с The Swift Developers/TSD_lesson_16-20/TSD_lesson_16-20/ViewControllers/SearchViewController/SearchViewController.swift
@@ -0,0 +1,17 @@
+//
+//  SearchViewController.swift
+//  TSD_lesson_16-20
+//
+//  Created by Антон Головатый on 09.02.2022.
+//
+
+import UIKit
+
+class SearchViewController: UIViewController {
+
+    @IBOutlet weak var searchBarTextField: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
