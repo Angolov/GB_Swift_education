58637b95ddf3dfee83f04e81c8de5d78825ce509
Angolov <anton-golovatyy@yandex.ru>
2022-02-24 20:14:43 +0300

    WeatherApp: выполнена методичка 4 по курсу GB Клиент-серверные приложения iOS

--- 3. Курс Клиент-серверные iOS-приложения/WeatherApp/WeatherApp/Services/WeatherService.swift
+++ 3. Курс Клиент-серверные iOS-приложения/WeatherApp/WeatherApp/Services/WeatherService.swift
@@ -7,6 +7,7 @@
 
 import Foundation
 import Alamofire
+import RealmSwift
 
 //MARK: - WeatherService class declaration
 final class WeatherService {
@@ -20,18 +21,30 @@ final class WeatherService {
         let parameters: Parameters = [
             "q": city,
             "appid": apiKey,
-            //"cnt": "1",
             "units": "metric"
         ]
         let url = baseUrl + path
         
         AF.request(url,
                    method: .get,
-                   parameters: parameters).responseData { response in
-            guard let data = response.value else { return }
-            let weather = try! JSONDecoder().decode(WeatherResponse.self, from: data).list
+                   parameters: parameters).responseData { [weak self] response in
+            guard let data = response.value,
+                  let self = self else { return }
             
+            let weather = try! JSONDecoder().decode(WeatherResponse.self, from: data).list
+            self.saveWeatherData(weather)
             completion(weather)
         }
+    }
+    
+    func saveWeatherData(_ weathers: [Weather]) {
+        do {
+            let realm = try Realm()
+            realm.beginWrite()
+            realm.add(weathers)
+            try realm.commitWrite()
+            
+        } catch {
+            print(error) }
     }
 }
